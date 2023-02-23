5d8fe35574718c89c0f9c14d91f092ed911830d0
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-02-23 19:55:01 +0700

    refactor: clear text field when enter is pressed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,6 +109,7 @@ export default function Home() {
             onKeyDown={(event) => {
               if (event.key === "Enter") {
                 sendMessage(message);
+                setMessage("");
               }
             }}
             InputProps={{
